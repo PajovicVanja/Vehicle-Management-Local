f1cc45f85cd5bf1a121f4e35cc29dcda10372e49
PajovicVanja <vanja.pajovic@student.um.si>
2024-11-03 19:59:42 +0100

    Removed sensitive data and created env file

--- backend/config/cloudinaryConfig.js
+++ backend/config/cloudinaryConfig.js
@@ -1,10 +1,9 @@
-// config/cloudinaryConfig.js
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({ 
-  cloud_name: 'dp1o8pfsn', // Replace with your Cloudinary cloud name
-  api_key: '749858336563791', // Replace with your Cloudinary API key
-  api_secret: 'FU78Z4jwioQ89l4jfX9lGzqIoEg' // Replace with your Cloudinary API secret
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
--- backend/config/firebaseConfig.js
+++ backend/config/firebaseConfig.js
@@ -1,10 +1,23 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+  throw new Error('Missing FIREBASE_PRIVATE_KEY in .env file');
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    }),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
   });
 }
 
