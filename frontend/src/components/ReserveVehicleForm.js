d6511e03fc4c8ac3d18591eb74a000fceef0eefd
Miloš Ančevski <miki2000milos@gmail.com>
2024-11-16 15:10:49 +0100

    added admin functionalities

--- frontend/src/components/ReserveVehicleForm.js
+++ frontend/src/components/ReserveVehicleForm.js
@@ -63,7 +63,19 @@ function ReserveVehicleForm({ token, reserveVehicleId, setReserveVehicleId, fetc
           minDate={new Date()} // Prevent selection of past dates
         />
         <br />
-        <button type="submit" className="add-vehicle-button">Reserve</button>
+        {canAddVehicle && (
+        <button onClick={() => setShowAddVehicle(true)} className="reserve-button">
+            Add Vehicle
+        </button>
+        )}
+        {canViewAllReservations && (
+        <button
+            onClick={() => setShowReserve(false)}
+            className="view-reservations-button"
+        >
+            View All Reservations
+        </button>
+        )}
       </form>
       <button onClick={() => setReserveVehicleId(null)} className='goto-vehicle-select-button'>Back to Vehicle selection</button>
     </div>
