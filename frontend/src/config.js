ec810b76aa43d2847a5e2a7bd02e8ca2c47f01c7
PajovicVanja <vanja.pajovic@student.um.si>
2025-10-05 17:58:59 +0200

    domain test

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-// super simple: same-origin by default; can still override in env
+// super simple: same-origin by default; can still override in envv
 const config = {
   getApiUrl: async () => process.env.REACT_APP_API_URL || '/api',
 };
