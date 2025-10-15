88bcb302a9f2b45803202ace38fd97a0d230daca
PajovicVanja <vanja.pajovic@student.um.si>
2025-10-15 08:09:06 +0200

    feat: refactor firebaseConfig

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,10 +1,18 @@
+// frontend/src/firebaseConfig.js
+const required = (name, val) => {
+  if (!val) {
+    throw new Error(`Missing ${name}. Did you set it in frontend/.env.local?`);
+  }
+  return val;
+};
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCkN8jy9U1CyiZFfvfS9msRIMomQgrME9k",
-    authDomain: "company-vehicle-management.firebaseapp.com",
-    projectId: "company-vehicle-management",
-    storageBucket: "company-vehicle-management.firebasestorage.app",
-    messagingSenderId: "246118144076",
-    appId: "1:246118144076:web:72c6742bf25f6573bbbce9"
-  };
+  apiKey: required('REACT_APP_FIREBASE_API_KEY', process.env.REACT_APP_FIREBASE_API_KEY),
+  authDomain: required('REACT_APP_FIREBASE_AUTH_DOMAIN', process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+  projectId: required('REACT_APP_FIREBASE_PROJECT_ID', process.env.REACT_APP_FIREBASE_PROJECT_ID),
+  storageBucket: required('REACT_APP_FIREBASE_STORAGE_BUCKET', process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+  messagingSenderId: required('REACT_APP_FIREBASE_MESSAGING_SENDER_ID', process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+  appId: required('REACT_APP_FIREBASE_APP_ID', process.env.REACT_APP_FIREBASE_APP_ID),
+};
 
 export default firebaseConfig;
